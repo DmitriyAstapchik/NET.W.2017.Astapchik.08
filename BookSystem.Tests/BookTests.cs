d781a31d66d40be34b8614df3f35fe3d068851a0
Dmitriy Astapchik <32869697+DmitriyAstapchik@users.noreply.github.com>
2017-11-13 18:06:53 +0300

    after remarks

--- BookSystem.Tests/BookTests.cs
+++ BookSystem.Tests/BookTests.cs
@@ -8,24 +8,16 @@ namespace BookSystem.Tests
     {
         private static Book book = new Book("978-0-7356-6745-7", "Jeffrey Richter", "CLR via C#", "Microsoft Press", DateTime.Parse("2012.1.1"), 826, 59.99m);
 
-        [TestCase("AT", ExpectedResult = "Jeffrey Richter, CLR via C#")]
-        [TestCase("ATBY", ExpectedResult = "Jeffrey Richter, CLR via C#, \"Microsoft Press\", 2012")]
-        [TestCase("IATBYP", ExpectedResult = "ISBN 13: 978-0-7356-6745-7, Jeffrey Richter, CLR via C#, \"Microsoft Press\", 2012, P. 826")]
-        [TestCase("IATBYPC", ExpectedResult = "ISBN 13: 978-0-7356-6745-7, Jeffrey Richter, CLR via C#, \"Microsoft Press\", 2012, P. 826, 59.99$")]
-        [TestCase("t", ExpectedResult = "CLR via C#")]
-        [TestCase("bYaI", ExpectedResult = "\"Microsoft Press\", 2012, Jeffrey Richter, ISBN 13: 978-0-7356-6745-7")]
-        [TestCase("taci", ExpectedResult = "CLR via C#, Jeffrey Richter, 59.99$, ISBN 13: 978-0-7356-6745-7")]
-        public string ToStringTest(string format)
+        [Test, TestCaseSource(typeof(BookTestsData), "ToStringTestCases")]
+        public string ToStringTest(string format, IFormatProvider provider)
         {
-            return book.ToString(format);
+            return book.ToString(format, provider);
         }
 
-        [TestCase("")]
-        [TestCase("w")]
-        [TestCase("ATW")]
-        public void ToStringExceptionTest(string format)
+        [Test, TestCaseSource(typeof(BookTestsData), "ToStringExceptionsTestCases")]
+        public void ToStringExceptionsTest(string format, IFormatProvider provider)
         {
-            Assert.That(() => book.ToString(format), Throws.Exception);
+            Assert.That(() => book.ToString(format, provider), Throws.Exception);
         }
 
         [TestCase("{0:atb}", ExpectedResult = "Jeffrey Richter, CLR via C#, \"Microsoft Press\"")]
